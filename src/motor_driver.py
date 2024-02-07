1f10857406663b158fef2acd447d4d67a341cfac
andyjwaideh <andyjwaideh@gmail.com>
2024-02-07 11:48:47 -0800

    Update motor_driver.py

--- src/motor_driver.py
+++ src/motor_driver.py
@@ -1,1 +1,8 @@
-motor_driver.py
+import MotorDriver
+
+
+if __name__ == '__main__':
+    print(MotorDriver)
+    moe = MotorDriver.motordriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, 3)
+    moe.set_duty_cycle (42)
+    
