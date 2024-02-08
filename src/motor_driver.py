b6a6f564a568158e674e7502ad99acd37b7c2a14
evoeddie <157066050+evoeddie@users.noreply.github.com>
2024-02-07 17:46:51 -0800

    Delete src directory

--- src/motor_driver.py
+++
@@ -1,9 +0,0 @@
-from MotorDriver import motordriver
-import pyb
-
-
-if __name__ == '__main__':
-    
-     moe = motordriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, 3)
-     moe.set_duty_cycle (50)
-    
